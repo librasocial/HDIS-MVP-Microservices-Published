017108a27de5a378af3a71adc04719b8914de937
letsdoc <36226808+letsdoc@users.noreply.github.com>
2024-03-20 15:21:16 +0530

    delete for ankith

--- hdis_consultation_plan/consultation_plan/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConsultationPlanConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'consultation_plan'
--- hdis_frontend/pharmacy_inventory_management/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PharmacyInventoryManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pharmacy_inventory_management'
